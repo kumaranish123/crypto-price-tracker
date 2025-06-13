76b9e579542d30bbe60a0adfb56ac0400ad00334
kumaranish123 <anisraj9128@gmail.com>
2025-06-13 17:56:04 +0530

    docs: add intro, setup, api, state & challenges pages

---
+++ docs/sidebars.js
@@ -0,0 +1,12 @@
+/** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
+const sidebars = {
+  tutorialSidebar: [
+    'intro',
+    'setup',
+    'api',
+    'state',
+    'challenges',
+  ],
+};
+
+module.exports = sidebars;
